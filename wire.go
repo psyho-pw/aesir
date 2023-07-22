14c9cfaac731184605fe2c2c222ae00fc1ad9bc1
sayho <fishcreek@naver.com>
2023-07-23 02:01:17 +0900

    feat: remove wire.go from build via build tag

--- wire.go
+++ wire.go
@@ -1,14 +1,17 @@
+//go:build wireinject
+// +build wireinject
+
 package main
 
-//import (
-//	"aesir/src"
-//	"github.com/gofiber/fiber/v2"
-//	"github.com/google/wire"
-//)
-//
-//var WireSet = wire.NewSet(src.AppSet)
-//
-//func New() (*fiber.App, error) {
-//	wire.Build(WireSet)
-//	return nil, nil
-//}
+import (
+	"aesir/src"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/wire"
+)
+
+var WireSet = wire.NewSet(src.AppSet)
+
+func New() (*fiber.App, error) {
+	wire.Build(WireSet)
+	return nil, nil
+}
